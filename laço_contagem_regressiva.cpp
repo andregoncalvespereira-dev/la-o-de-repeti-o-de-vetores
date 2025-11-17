3a691f700e150dde578fdbcd792390797cccf63c
André Gonçalves Pereira <andre.goncalves.pereira@escola.pr.gov.br>
2025-11-17 14:16:18 -0300

    Update laço_contagem_regressiva.cpp

--- laço_contagem_regressiva.cpp
+++ laço_contagem_regressiva.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main() {
 
-    for (int i = 100; i >= 1; --i) {
+    for (int i = 100; i >= 1; i--) {
         cout << i << " ";
     }
-
-    cout << endl;
+    
     return 0;
+}
